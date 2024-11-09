b9fdf1ee1718bb90dcd2fdb97bdbe6bf95a22d14
VoMKhang <khangpro590@gmail.com>
2024-11-10 06:27:20 +0700

    khangvm - add auth

--- src/redux/slices/apiSlice.js
+++ src/redux/slices/apiSlice.js
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-const base_url_api = process.env.REACT_APP_BACKEND_API_URL // config .env
+const base_url_api = process.env.REACT_APP_BACKEND_API_URL 
 const base_query = fetchBaseQuery({ baseUrl: base_url_api })
 
 export const apiSlice = createApi({
