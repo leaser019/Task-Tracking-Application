a1a74b21ce9dcb9aab335c8856b775de9cab64b0
VoMKhang <khangpro590@gmail.com>
2024-12-08 15:35:34 +0700

    Refactor TableHeader component for improved styling and layout; center align headers and add new 'Action' column

--- src/components/common/table/TableHeader.jsx
+++ src/components/common/table/TableHeader.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import Application from '../../../pages/Application'
 
 const TableHeader = () => {
   return (
-    <thead className="w-full border-b border-gray-300">
-      <tr className="w-full text-black  text-left">
-        <th className="py-2">Application Title</th>
-        <th className="py-2">Priority</th>
-        <th className="py-2 line-clamp-1">Created At</th>
-        <th className="py-2">Assets</th>
+    <thead className="w-full border-b border-gray-300 ">
+      <tr className="w-full text-black  text-left ">
+        <th className="py-2 line-clamp-1 ">Application Title</th>
+        <th className="py-2 text-center pr-6">Priority</th>
+        <th className="py-2 line-clamp-1 text-center">Created At</th>
+        <th className="py-2 text-center pr-8">Assets</th>
         <th className="py-2">Team</th>
+        <th className="py-2 text-center">Action</th>
       </tr>
     </thead>
   )
